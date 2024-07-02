f37a88a8c81e667beeca9283839216f2493e1cec
M.Atif Afridi <atifu4671@gmail.com>
2024-07-02 16:55:20 -0700

    firstproject

---
+++ HostingInReact/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+ 
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+      <h1>heading1</h1>
+      <h2>heading2</h2>
+      
+    </>
+  )
+}
+
+export default App
